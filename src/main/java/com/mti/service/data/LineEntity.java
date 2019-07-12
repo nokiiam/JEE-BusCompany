2a751011b6d5c6712ec5a0e6e792ee5e12237744
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 15:29:04 +0200

    Added LineEntity

---
+++ src/main/java/com/mti/service/data/LineEntity.java
@@ -0,0 +1,17 @@
+package com.mti.service.data;
+
+public class LineEntity extends Entity {
+    private Integer number;
+
+    public LineEntity() {
+
+    }
+
+    public Integer getName() {
+        return number;
+    }
+
+    public void setName(Integer number) {
+        this.number = number;
+    }
+}
