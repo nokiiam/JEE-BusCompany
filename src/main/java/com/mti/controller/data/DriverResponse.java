1aa00dcf62dd057a5611c3419c224893d2361ba9
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-12 15:34:50 +0200

    Adding Driver Request and Response

---
+++ src/main/java/com/mti/controller/data/DriverResponse.java
@@ -0,0 +1,55 @@
+package com.mti.controller.data;
+
+import java.util.Date;
+
+public class DriverResponse extends Response {
+
+    private String code;
+    private String firstname;
+    private String lastname;
+    private Date birth_date;
+    private Integer status;
+
+    public DriverResponse() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
