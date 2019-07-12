a10541a520abfdc90b91a07d687a939b70969815
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 14:55:21 +0200

    Updtated scripts

--- scripts/buscompanydb.sql
+++ scripts/buscompanydb.sql
@@ -42,7 +42,8 @@ CREATE TABLE IF NOT EXISTS `buscompanydb`.`bus_line` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `number` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `number_UNIQUE` (`number` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
