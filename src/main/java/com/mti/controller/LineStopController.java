b6b981f4eabf02401d014d94572978c63f1352a3
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 20:25:39 +0200

    Added LineStopController

---
+++ src/main/java/com/mti/controller/LineStopController.java
@@ -0,0 +1,39 @@
+package com.mti.controller;
+
+import com.mti.controller.converter.ControllerEntityConverter;
+import com.mti.controller.converter.LineStopControllerEntityConverter;
+import com.mti.controller.data.LineStopRequest;
+import com.mti.controller.data.LineStopResponse;
+import com.mti.model.dao.LineStopDao;
+import com.mti.model.data.LineStopModel;
+import com.mti.service.LineStopService;
+import com.mti.service.data.LineStopEntity;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+
+/**
+ * Controller for lines
+ * Handles requests to '/stops'
+ */
+@RequestScoped
+@Path("/stops")
+public class LineStopController implements Controller<LineStopRequest, LineStopResponse, LineStopEntity, LineStopModel, LineStopDao, LineStopService> {
+
+    @Inject
+    private LineStopService service;
+    @Inject
+    private LineStopControllerEntityConverter converter;
+
+    @Override
+    public LineStopService getService() {
+        return service;
+    }
+
+    @Override
+    public ControllerEntityConverter<LineStopRequest, LineStopResponse, LineStopEntity> getConverter() {
+        return converter;
+    }
+}
