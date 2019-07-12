a9dea11ee52c5501b84862504116b64510cbc102
Romain Mounier <romain.mounier@epita.fr>
2019-07-12 14:29:37 +0200

    Create Driver and Stop model

--- src/main/java/com/mti/model/data/StopModel.java
+++ src/main/java/com/mti/model/data/StopModel.java
@@ -1,9 +1,29 @@
 package com.mti.model.data;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "bus_stop")
 public class StopModel extends Model {
 
+    @Column(name = "`name`", unique = true)
+    private String name;
+
+    public StopModel() {
+    }
+
+    public StopModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
