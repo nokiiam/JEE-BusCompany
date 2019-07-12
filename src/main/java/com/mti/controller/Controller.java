428e60b80e5925a8c02be85bdee79fce97ef99e5
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 12:11:33 +0200

    Refactored generics names

--- src/main/java/com/mti/controller/Controller.java
+++ src/main/java/com/mti/controller/Controller.java
@@ -7,7 +7,7 @@ import com.mti.model.data.Model;
 import com.mti.service.Service;
 import com.mti.service.data.Entity;
 
-interface Controller<REQUEST_MODEL_TYPE extends Request, RESPONSE_MODEL_TYPE extends Response, ENTITY_TYPE extends Entity, MODEL_TYPE extends Model,
+interface Controller<REQUEST_TYPE extends Request, RESPONSE_TYPE extends Response, ENTITY_TYPE extends Entity, MODEL_TYPE extends Model,
         DAO_TYPE extends Dao<MODEL_TYPE>,
         SERVICE_TYPE extends Service<ENTITY_TYPE, MODEL_TYPE, DAO_TYPE>> {
 }
--- src/main/java/com/mti/controller/converter/ControllerEntityConverter.java
+++ src/main/java/com/mti/controller/converter/ControllerEntityConverter.java
@@ -4,9 +4,9 @@ import com.mti.service.data.Entity;
 import com.mti.controller.data.Request;
 import com.mti.controller.data.Response;
 
-public interface ControllerEntityConverter<REQUEST_MODEL_TYPE extends Request, RESPONSE_MODEL_TYPE extends Response, ENTITY_TYPE extends Entity> {
+public interface ControllerEntityConverter<REQUEST_TYPE extends Request, RESPONSE_TYPE extends Response, ENTITY_TYPE extends Entity> {
 
-    ENTITY_TYPE controllerToEntity(REQUEST_MODEL_TYPE model);
+    ENTITY_TYPE controllerToEntity(REQUEST_TYPE model);
 
-    RESPONSE_MODEL_TYPE entityToController(ENTITY_TYPE entity);
+    RESPONSE_TYPE entityToController(ENTITY_TYPE entity);
 }
