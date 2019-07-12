c0af3254eac1a5c401a5c59c80514ef5972f72e3
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-12 16:17:28 +0200

    Adding SlotEntity

---
+++ src/main/java/com/mti/service/data/SlotEntity.java
@@ -0,0 +1,55 @@
+package com.mti.service.data;
+
+import java.util.Date;
+
+public class SlotEntity {
+
+    private Date date;
+    private Boolean isMorning;
+    private DriverEntity driver;
+    private BusEntity bus;
+    private LineEntity line;
+
+    public SlotEntity() {
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getMorning() {
+        return isMorning;
+    }
+
+    public void setMorning(Boolean morning) {
+        isMorning = morning;
+    }
+
+    public DriverEntity getDriver() {
+        return driver;
+    }
+
+    public void setDriver(DriverEntity driver) {
+        this.driver = driver;
+    }
+
+    public BusEntity getBus() {
+        return bus;
+    }
+
+    public void setBus(BusEntity bus) {
+        this.bus = bus;
+    }
+
+    public LineEntity getLine() {
+        return line;
+    }
+
+    public void setLine(LineEntity line) {
+        this.line = line;
+    }
+}
