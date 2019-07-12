71d953b09ed1f08323a37989ed7a008696d5d2ad
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-12 16:13:35 +0200

    Add DriverEntity

---
+++ src/main/java/com/mti/service/data/DriverEntity.java
@@ -0,0 +1,55 @@
+package com.mti.service.data;
+
+import java.util.Date;
+
+public class DriverEntity {
+
+    private String code;
+    private Date birthDate;
+    private String firstname;
+    private String lastname;
+    private int status;
+
+    public DriverEntity() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
