f1b1b9235c325ab4f4abc6bd5ebe9fb1574a59bc
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-12 15:43:01 +0200

    Adding BusEntity

---
+++ src/main/java/com/mti/service/data/BusEntity.java
@@ -0,0 +1,55 @@
+package com.mti.service.data;
+
+import java.util.Date;
+
+public class BusEntity extends Entity {
+
+    private String registration;
+    private String brand;
+    private Integer capacity;
+    private Integer kilometers;
+    private Date release_time;
+
+    public BusEntity() {
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(Integer kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public Date getRelease_time() {
+        return release_time;
+    }
+
+    public void setRelease_time(Date release_time) {
+        this.release_time = release_time;
+    }
+}
