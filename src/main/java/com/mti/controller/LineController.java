ac9bd32246ae7fc56a3696fe587e2d5cd143c355
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 16:18:08 +0200

    Completed LineController

--- src/main/java/com/mti/controller/LineController.java
+++ src/main/java/com/mti/controller/LineController.java
@@ -1,4 +1,30 @@
 package com.mti.controller;
 
-public class LineController {
+import com.mti.controller.converter.ControllerEntityConverter;
+import com.mti.controller.converter.LineControllerEntityConverter;
+import com.mti.controller.data.LineRequest;
+import com.mti.controller.data.LineResponse;
+import com.mti.model.dao.LineDao;
+import com.mti.model.data.LineModel;
+import com.mti.service.LineService;
+import com.mti.service.data.LineEntity;
+
+import javax.inject.Inject;
+
+public class LineController implements Controller<LineRequest, LineResponse, LineEntity, LineModel, LineDao, LineService> {
+
+    @Inject
+    private LineService service;
+    @Inject
+    private LineControllerEntityConverter converter;
+
+    @Override
+    public LineService getService() {
+        return service;
+    }
+
+    @Override
+    public ControllerEntityConverter<LineRequest, LineResponse, LineEntity> getConverter() {
+        return converter;
+    }
 }
