29d7782b28a336f2c1a81442d5742218ffc2e4f1
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-12 12:03:39 +0200

    Added entension on generics

--- src/main/java/com/mti/controller/converter/ControllerEntityConverter.java
+++ src/main/java/com/mti/controller/converter/ControllerEntityConverter.java
@@ -1,8 +1,10 @@
 package com.mti.controller.converter;
 
 import com.mti.service.data.Entity;
+import com.mti.controller.data.Request;
+import com.mti.controller.data.Response;
 
-public interface ControllerEntityConverter<REQUEST_MODEL_TYPE, RESPONSE_MODEL_TYPE, ENTITY_TYPE extends Entity> {
+public interface ControllerEntityConverter<REQUEST_MODEL_TYPE extends Request, RESPONSE_MODEL_TYPE extends Response, ENTITY_TYPE extends Entity> {
 
     ENTITY_TYPE controllerToEntity(REQUEST_MODEL_TYPE model);
 
