73ed32f779002766c60e0bfd28b165d6c2771c95
Hedgestock <arthur.pamart@epita.fr>
2019-07-12 17:06:42 +0200

    Made bus stop name unique butterfly

--- scripts/buscompanydb.sql
+++ scripts/buscompanydb.sql
@@ -53,10 +53,12 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `buscompanydb`.`bus_stop`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `buscompanydb`.`bus_stop` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+                                                         `id` INT(11) NOT NULL AUTO_INCREMENT,
+                                                         `name` VARCHAR(45) NOT NULL,
+                                                         PRIMARY KEY (`id`),
+                                                         UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+                                                         UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
