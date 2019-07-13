954ccd214e03658c16d2ddd312ba00d8771d0de6
Anthony Truong <anthonytruong@hotmail.fr>
2019-07-13 14:40:13 +0200

    Added bus page management

--- src/main/java/com/mti/controller/BusController.java
+++ src/main/java/com/mti/controller/BusController.java
@@ -11,7 +11,12 @@ import com.mti.service.data.BusEntity;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -20,7 +25,14 @@ import javax.ws.rs.Path;
  */
 @RequestScoped
 @Path("/buses")
+@Named
 public class BusController implements Controller<BusRequest, BusResponse, BusEntity, BusModel, BusDao, BusService> {
+
+    private String brand;
+    private String registration;
+    private Integer capacity;
+    private Integer kilometers;
+    private String releaseYear;
 
     @Inject
     private BusService service;
@@ -35,5 +47,60 @@ public class BusController implements Controller<BusRequest, BusResponse, BusEnt
     @Override
     public ControllerEntityConverter<BusRequest, BusResponse, BusEntity> getConverter() {
         return converter;
+    }
+
+    public Response formCreate() {
+        SimpleDateFormat formatter =new SimpleDateFormat("yyyy-MM-dd");
+        BusRequest busRequest = new BusRequest();
+        busRequest.setBrand(brand);
+        busRequest.setRegistration(registration);
+        busRequest.setCapacity(capacity);
+        busRequest.setKilometers(kilometers);
+        try {
+            busRequest.setReleaseYear(formatter.parse(releaseYear));
+        } catch (ParseException e) {
+            return null;
+        }
+        return create(busRequest);
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setKilometers(Integer kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public Integer getKilometers() {
+        return kilometers;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
     }
 }
